1643e5aa5557f5150f64c910adad7371be10abef
Sebastian Rossi <rossis@macbook-pro-715.mynetworksettings.com>
2025-09-07 16:17:52 -0400

    Initial react setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="under-construction">
+      <div className="construction-content">
+        <h1>🚧 Under Construction 🚧</h1>
+        <p>This website is currently being built and will be available soon!</p>
+        <p>Check back later for updates.</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
